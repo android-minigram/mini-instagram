e29cd66d171e5d1de5f042ef7d0a1f53e4b2e47f
Mohamed Tawfik <motawfik98@outlook.com>
2021-01-02 01:06:02 +0200

    Added app icon with splash screen

--- app/src/main/java/com/motawfik/minigram/MainActivity.kt
+++ app/src/main/java/com/motawfik/minigram/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.motawfik.minigram
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.NavHostFragment
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Minigram)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
